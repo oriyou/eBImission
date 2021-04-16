2a2c5ccdc7ee4ef78cfe49784ff44dda952ce907
unknown <fldhflsj1@gmail.com>
2021-04-16 10:20:02 +0900

    Fix WebClient using config

--- eBImission/src/main/java/com/example/eBImission/entity/dto/CartProductDto.java
+++ eBImission/src/main/java/com/example/eBImission/entity/dto/CartProductDto.java
@@ -1,5 +1,6 @@
 package com.example.eBImission.entity.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CartProductDto {
     private int cartSn;
     private String trNo;
--- eBImission/src/main/java/com/example/eBImission/service/impl/CartServiceImpl.java
+++ eBImission/src/main/java/com/example/eBImission/service/impl/CartServiceImpl.java
@@ -1,11 +1,13 @@
 package com.example.eBImission.service.impl;
 
+import com.example.eBImission.config.WebClientConfig;
 import com.example.eBImission.entity.Cart;
 import com.example.eBImission.entity.dto.CartProductDto;
 import com.example.eBImission.service.CartService;
 import com.example.eBImission.repository.CartRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 
@@ -24,14 +26,17 @@ import java.time.LocalDateTime;
 public class CartServiceImpl implements CartService {
 
     private final CartRepository cartRepository;
+    private final WebClient webClient;
 
     @Override
     public Flux<CartProductDto> retrieveCart() {
 
         final String END_POINT_URI = "https://pbf.lotteon.com/product/v1/detail/productDetailList?dataType=LIGHT2";
 //        Flux<Cart> cart = cartRepository.findAll();
+        ObjectMapper mapper = new ObjectMapper();
         Flux<Cart> cart = cartRepository.findAllOrderByRegDttm();
 
+        /*
         WebClient webClient = WebClient.create();
         Flux<CartProductDto> productInfo = webClient.post()
                         .uri(END_POINT_URI)
@@ -43,6 +48,23 @@ public class CartServiceImpl implements CartService {
                         .map(e -> e.path("data"))
                         .flatMapMany(Flux::fromIterable)
                         .map(e -> new Gson().fromJson(String.valueOf(e), CartProductDto.class));
+        */
+
+
+        // Webclient의 기존 설정값을 상속해서 사용할 수 있는 mutate() 함수를 통해 build()
+        Flux<CartProductDto> productInfo = webClient.mutate()
+                        .build()
+                        .post()
+                        .uri(END_POINT_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(cart, Cart.class))
+                        .retrieve()
+                        .bodyToMono(JsonNode.class)
+                        .map(e -> e.path("data"))
+                        .flatMapMany(Flux::fromIterable)
+                        .map(e -> mapper.convertValue(e, CartProductDto.class));
+
 
         // cartSn, odQty, mbNo 추가
         return productInfo.zipWith(cart, (product, cartProduct) -> {
